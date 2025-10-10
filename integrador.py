d1aaf37c01b347ac3eaf4db59ef5009bef135a3b
eEmanuel07 <elias.e.tello1@gmail.com>
2025-10-10 19:36:47 -0300

    primera actualización de archivos

---
+++ integrador.py
@@ -0,0 +1,28 @@
+import mis_funciones
+import pandas as pd
+
+
+menu = ("------ Menu ------\n\n"
+    "1. Buscar pais por nombre\n"
+    "2. Filtro de paises\n"
+    "3. Ordenar lista de paises\n"
+    "4. Mostrar estadisticas\n"
+    "5. Sair\n"
+    #agregar paises
+    #guardar modificaciones
+)
+opcion = 0
+
+while opcion != 5:
+    print(menu)
+    opcion = input("Ingrese una opción: ")
+    match opcion:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+
