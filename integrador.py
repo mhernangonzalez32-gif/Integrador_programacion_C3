e55d300416bc73df565da315331c39848e935319
mhernangonzalez32-gif <mhernangonzalez32@gmail.com>
2025-10-20 11:05:11 -0300

    Algo

--- integrador.py
+++ integrador.py
@@ -1,32 +1,30 @@
 import mis_funciones
-import csv
-#import pandas as pd
+import filtro_de_busqueda
+import busqueda_por_nombre
 
-with open('paises_mundo.csv', 'w', newline="") as archivo:
-    escritor = csv.writer(archivo)
-    
+
 menu = ("------ Menu ------\n\n"
     "1. Buscar pais por nombre\n"
     "2. Filtro de paises\n"
     "3. Ordenar lista de paises\n"
     "4. Mostrar estadisticas\n"
     "5. Sair\n"
-    #agregar paises
-    #guardar modificaciones
 )
-opcion = 0
 
-while opcion != 5:
+paises = mis_funciones.cargar_datos_desde_csv("paises_mundo.csv")
+
+while True:
     print(menu)
-    opcion = input("Ingrese una opción: ")
-    opcion_int = mis_funciones.verificar_num(opcion)
-    match opcion_int:
+    opcion = mis_funciones.numero_opcion(5)
+    match opcion:
         case 1:
-            pass
+            nombre_pais = input("Que pais esta buscando?\n")
+            print(busqueda_por_nombre.buscar_pais_nombre(paises, nombre_pais))
         case 2:
-            pass
+            filtro_de_busqueda.buscar_por_continente(paises)
         case 3:
             pass
         case 4:
-            break
-
+            pass
+        case 5:
+            print("Adios")
